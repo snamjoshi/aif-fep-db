d86140ec7952e44857797e2c39ea831e41c117d4
Sanjeev Namjoshi <snamjoshi87@utexas.edu>
2023-09-14 11:43:44 -0500

    Basic tag class structure

--- src/tags.py
+++ src/tags.py
@@ -10,12 +10,20 @@ class Tags:
         pass
     
     def create_tag_file(self, path):
+        """ Creates a new tag file """
+        # Possibility for tag groups? One can specify the tag group when creating a tag
+        # This tag group allows you to group different types of tags with similar values together
+        # A tag file always has a tag list that one can choose from when adding tags
+        # If tag is not there, it must be added to the tag list first.
+        # Start backward from a tag file: What the structure is and load it.
+        # Work backward to figure out what an empty version looks like
+        empty_tags = {}
     
     def load_tags(self):
         """ A tag file should consist of a PMID and a set of tags"""
         self._check_db_exists()
         
-    def add_tags(self):
+    def add_new_list(self):
         # TODO
         self._check_db_exists()
         # Check list of available tags
@@ -23,5 +31,6 @@ class Tags:
         # Adding a tag will write to the tag file. 
         
     def add_tags_interactive(self):
+        """ Interactive tag adding mode"""
         # TODO
         self._check_db_exists()
